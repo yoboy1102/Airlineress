b53a6bb094cda7b9e1c58287b22fefd5d5a27762
yoboy1102 <156419541+yoboy1102@users.noreply.github.com>
2024-09-24 13:33:53 +0400

    Update SeatReservation.py

--- Project/SeatReservation.py
+++ Project/SeatReservation.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageTk
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 from TopMenu import TopMenu
+from SeatResFunc import SeatResFunc
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"c:\Users\Arubaaa\OneDrive\Desktop\DESKTOP\kama 2024\build\assets\frame0")
@@ -13,17 +14,23 @@ ASSETS_PATH = OUTPUT_PATH / Path(r"c:\Users\Arubaaa\OneDrive\Desktop\DESKTOP\kam
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
-def radd(x , m, n):
-    pass
-
-
-
-
 def SeatRes(window):
-    def CBcheck_val(p, q):
-        print(p, q)
-        print(eco_rad[p])
-        for i in eco_rad:
+    def CBcheck_val(p, q, er):
+        print(len(er))
+        print(p+1, let_list[q])
+        for h in er:
+            for i in h:
+                if i == er[p][q]:
+                    print(i, i.get())
+                    print('a')
+                    pass
+                elif i!= er[p][q]:
+                    if i.get() == 1:
+                        print(i, i.get())
+                        i.deselect()    
+        er[p][q].configure(fg_color='#ED5E7A')
+        for i in er:
+            break
             print(i)
     canvas_2 = Canvas(
         window,
@@ -63,7 +70,7 @@ def SeatRes(window):
                            fg_color='#EAEBFF',
                            bg_color='#EAEBFF',
                            )
-    all_frm.place(x=417-20, y=505)
+    all_frm.place(x=417-34, y=505)
     eco_buss, w_h = [], [312, 420, 470]
     for i in range(3):
         v = ctk.CTkFrame(all_frm,
@@ -74,50 +81,16 @@ def SeatRes(window):
                            )
         v.grid(row=i, column=0, padx=4, pady=20)
         eco_buss.append(v)
+    eco_rad,bus_rad = [],[]
+
     let_list = [' A', 'B ', ' C ', 'D ', ' E', ' F ']
 
     n, m=4, 6    
 
     var = ctk.IntVar()
-    eco_rad = []
-    k,l=0, 0
-    for i in range(m):
-        ff=0
-        v = []
-        let_new = let_list[0:n]
-        let_new.insert(int(n/2), '    ')
-        for j in range(n):
-            
-            var = ctk.IntVar(eco_buss[0])  
-            ff+=1.4
-            
-            #print(ff, int(ff), int(ff-1))
-            radd = ctk.CTkCheckBox(eco_buss[0],
-                                  corner_radius=8, fg_color='#3DCCB2', text=None,
-                                  variable=var, 
-                                  onvalue=None,
-                                  border_color='#3DCCB2', checkbox_height=43, checkbox_width=33,
-                                  height=20, width=0, command=lambda: CBcheck_val(k, l))
-            #vED5E7A
-            radd.grid(row=i+1, column=int(ff-1), padx=3, pady=10)
-            v.append(radd)
-            v.append(l)
-            l+=1  
-            if i==0:
-                le_lbll =  ctk.CTkLabel(eco_buss[0],
-                                    text=let_new[int(ff-1)], font=('georgia', 14, 'bold'),
-                                    #text_color=''
-                                    )
-                le_lbll.grid(row=i, column=int(ff-1), sticky='s')  
-        n0_lbll = ctk.CTkLabel(eco_buss[0], text=str(i+1)+'  ',font=('georgia', 14, 'bold'))
-        n0_lbll.grid(row=i+1, column=2)
 
-        emp_label = ctk.CTkLabel(eco_buss[0], text=let_new[int(n/2)])
-        emp_label.grid(row=0, column=(int(n/2)))
-
-        eco_rad.append(v)
-        eco_rad.append(k)
-        k+=1
+    SeatResFunc(6, 4, eco_buss[0], CBcheck_val, 1.4, 33, bus_rad)
+    SeatResFunc(8, 6, eco_buss[1], CBcheck_val, 1.3, 26, eco_rad)
     TopMenu(window, canvas_2, -50)
     
 web=ctk.CTk()
