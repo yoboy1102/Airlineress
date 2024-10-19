b7f3ea8c871eaf9e597c8f5fdb69f51c515bb608
yoboy1102 <156419541+yoboy1102@users.noreply.github.com>
2024-10-19 09:26:34 +0400

    Update PassengerDetails.py

--- Project/PassengerDetails.py
+++ Project/PassengerDetails.py
@@ -27,9 +27,9 @@ def PassDetails(window, list_no_pass, count_pass):
 
     canvas_3 = Canvas(window, bg = "#D9D9D9", bd = 0,  height = 1000, width = 1880, highlightthickness = 0)
     canvas_3.place(x = 0, y = 0)
+
     H_W, H_H = 1600, 180
     V_W, V_H = 1880-H_W+2, 1000-92
-
     horiz_top = CTkFrame(window,
                          height=H_H, width=H_W,
                          fg_color='#071B41', corner_radius=0)
@@ -42,26 +42,17 @@ def PassDetails(window, list_no_pass, count_pass):
     vert_top.pack_propagate(False)
 
     S_H, S_W =1000-92-H_H, 1880-V_W
-
     pass_sclr_frame = CTkScrollableFrame(window, height=S_H, width=S_W, fg_color='#3860A1',
                                           border_color='#CCD09F', border_width=0, corner_radius=0)#223A60
     pass_sclr_frame.place(x=0, y=92+H_H)
 
-    
     lineimg = CTkImage(light_image=Image.open(relative_to_assets('Line 1.png')),
                            dark_image=Image.open(relative_to_assets('Line 1.png')),
                            size=(3, S_H))
     linelbl = CTkLabel(window, image=lineimg, fg_color='transparent', text='')
     linelbl.place(x=S_W/2, y=92+H_H)
-    #canvas_4 = Canvas(window, bd=0, height=S_H, width=4, background='grey', bg='grey',)
-    #canvas_3.create_line(S_W/2-3, S_H, S_W/2+2, 1000, width=3)
-    #canvas_4.create_line(2, 5, 2, S_H, width=7, fill='grey')
-    #canvas_4.place(x=S_W/2, y=92+H_H)
-    #canvas_4.pack()
 
-    frm_c = CTkFrame(horiz_top,
-                         #height=1, width=1,
-                               fg_color='transparent')
+    frm_c = CTkFrame(horiz_top, fg_color='transparent')
     frm_c.grid(row=0, column=0, sticky='w', padx=85, pady=35)
     p_head_text_l = ['Passenger Information',
                  '''   Enter the required information for each traveler, and be sure                 \nthat it exactly matches the government-issued ID presented at the airport.''']
@@ -82,48 +73,47 @@ def PassDetails(window, list_no_pass, count_pass):
     update_time()
 
     emp_space = CTkFrame(vert_top, height=100, fg_color='transparent'   )
-    emp_space.pack(pady=30)
+    emp_space.pack(pady=35)
     emp_space_2 = CTkFrame(vert_top, height=100, fg_color='transparent')
-    emp_space_2.pack(pady=20)
+    #emp_space_2.pack(pady=20)
 
-    luggage_lim = CTkFrame(vert_top, #fg_color='#071B41'
+    luggage_lim = CTkFrame(vert_top, fg_color='#071B41'
                            )
     luggage_lim.pack(pady=10, padx=10)
 
-    luggage_lim_lbl = CTkLabel(luggage_lim, text='Luggage Limit', font=('Georgia', 18, 'bold'), 
-                        #text_color='#A6ACAC'
+    luggage_lim_lbl = CTkLabel(luggage_lim, text='Luggage Limit', font=('Georgia', 22, 'bold'), 
+                        text_color='#A6ACAC'
                         )
     luggage_lim_lbl.pack()
 
     luggage_lim_frm = CTkFrame(luggage_lim, fg_color='transparent')
-    luggage_lim_frm.pack(pady=10, padx=10)
+    luggage_lim_frm.pack(pady=15, padx=10)
 
     lug_txt_amt = ['20 Kg', '10 Kg', '5 Kg']
-    lug_txt = ['Adult', 'Child', 'Toddler']
+    lug_txt = [' Adult', ' Child', ' Toddler']
 
     for i in range(3):
         
-        
-        lug_txt_i = CTkLabel(luggage_lim_frm, text=lug_txt[i], font=('Georgia', 18, 'bold'), 
-                        #text_color='#A6ACAC'
+        lug_txt_i = CTkLabel(luggage_lim_frm, text=lug_txt[i], font=('ink free', 20, 'bold'), 
+                              text_color='#A6ACAC'
                         )
-        lug_txt_i.grid(row=0, column=i, sticky='w', padx=10 ,pady=10)
-    for i in range(3):
-
-        lug_txt_amt_i = CTkLabel(luggage_lim_frm, text=lug_txt_amt[i], font=('Georgia', 18, 'bold'), 
-                        #text_color='#A6ACAC'
+        lug_txt_i.grid(row=i, column=0, sticky='w', padx=5 ,pady=15)
+        
+        arr_text_i = CTkLabel(luggage_lim_frm, text = ' --> ', font=('georgia', 18, 'bold'),
+                              text_color='#A6ACAC'
+                              )
+        arr_text_i.grid(row=i, column=1, sticky='w', padx=5 ,pady=15)
+                        
+        lug_amt_i = CTkLabel(luggage_lim_frm, text=lug_txt_amt[i], font=('ink free', 20, 'bold'), 
+                              text_color='#A6ACAC'
                         )
-        lug_txt_amt_i.grid(row=1, column=i, sticky='w', padx=10 ,pady=10)
-        
-        
-
-
+        lug_amt_i.grid(row=i, column=2, sticky='w', padx=5 ,pady=15)
+         
     luggage_img = CTkImage(light_image=Image.open(relative_to_assets('Luggage.png')),
                            dark_image=Image.open(relative_to_assets('Luggage.png')),
-                           size=(V_W-10, (V_W-10)*1.2))
+                           size=(V_W-10, (V_W-5)*1.2))
     luggage_lbl = CTkLabel(vert_top, image=luggage_img, fg_color='transparent', text='')
-    luggage_lbl.pack(pady=35)
-
+    luggage_lbl.pack(pady=10)
 
     continue_btn_font=ctk.CTkFont('Georgia', 18, 'bold', slant='italic')
     continue_btn = ctk.CTkButton(vert_top ,width=175, height=60, corner_radius=10, border_width=7, hover=False,
@@ -133,24 +123,18 @@ def PassDetails(window, list_no_pass, count_pass):
     
     continue_btn.pack(pady=10, padx=20)
 
-    list_frm=[]
-    fgbb = '#D9EBE0'
-    fgbb='transparent'
-    nsew='ns'
-    pdx=65
+    pdx, nsew, list_frm=65, 'ns', []
     for i in range(2):
 
-        frm_l_r = CTkFrame(pass_sclr_frame, fg_color=fgbb, height=1000)
+        frm_l_r = CTkFrame(pass_sclr_frame, fg_color='transparent', height=1000)
         frm_l_r.grid(row=0, column=i, sticky=nsew, padx=pdx, pady=40)
         list_frm.append(frm_l_r)
     
     txt_l_0 = ['Adult', 'Child', 'Toddler']
     no_adults, no_children, no_toddlers = list_no_pass[0], list_no_pass[1], list_no_pass[2]
     no_l =[no_adults, no_children, no_toddlers]
-    k0 = 0
-    cc,ccc=0, 0
+    cc,k0=0, 0
     frm_m,bb,pas_l,ent_list=[],[],[],[]
-
 
     for j in range(3):
         print('j',j, txt_l_0[j])
@@ -170,7 +154,6 @@ def PassDetails(window, list_no_pass, count_pass):
         print(bbb)
         bb.append(bbb)
 
-        
         for k in range(no_l[j]):
                 cc+=1
                 print(len(bb),'k',k, bb[j][k], bb[j][k])
@@ -180,19 +163,15 @@ def PassDetails(window, list_no_pass, count_pass):
                 frm_head.pack(pady=10, fill='x', padx=10)
 
                 font_head = CTkFont('georgia', 20, 'bold', underline=True)
-                pas_lbl_0 = CTkLabel(frm_head,
-                                         text=txt_pas,
-                                         font=font_head, text_color='black')
+                pas_lbl_0 = CTkLabel(frm_head, text=txt_pas,font=font_head, text_color='black')
                 pas_lbl_0.grid(row=0, column=0, sticky='w')
                 pas_l.append(pas_lbl_0)
 
-
-                font_body=('Times',16, )
+                font_body=('Times',16)
                 ent_text_l1 = ['First Name*','Middle Name', 'Last Name*']
                 ent_text_l2 = ['DOB', 'Email Address*']
                 title_var = ctk.StringVar()
                 title_vals = ['Mr', 'Mrs', 'Miss', 'Ms','Undisclosed']
-
 
                 frm_body = CTkFrame(frm_m[j][k], fg_color='transparent')
                 frm_body.pack(pady=10)
@@ -202,15 +181,11 @@ def PassDetails(window, list_no_pass, count_pass):
                                           variable=title_var, button_color='#A1B0CC', border_color='#A1B0CC' )
                 title_m.grid(row=0, column=0, sticky='w', padx=10, pady=10)
                 
-
                 ent_emp =[]
                 for i in range(3):
-                      ent = CTkEntry(frm_body, 
-                                         placeholder_text=ent_text_l1[i],
-                                         height=s_hei, width=s_wid,
-                                         font=('Helvetica', 14), corner_radius=6,
-                                         border_color='#A1B0CC', border_width=1,
-                                         fg_color='#D8EADF'
+                      ent = CTkEntry(frm_body, placeholder_text=ent_text_l1[i], height=s_hei, width=s_wid,
+                                         font=('Helvetica', 14), border_color='#A1B0CC', fg_color='#D8EADF', 
+                                         corner_radius=6, border_width=1,
                                          )
                       ent.grid(row=1, column=i, padx=10, pady=5)
                       ent_emp.append(ent)
